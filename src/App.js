257dc3d3b56280882bdb109389f85ed6feb948d0
Adonis Mendoza <amendoza@arsmetasalud.com>
2021-07-14 19:25:21 -0400

    [Create] Project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+        Signature Generator.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
